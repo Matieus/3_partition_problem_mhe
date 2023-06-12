6394c48e2765eeb6ce85c76d920ed74c55f8f91c
Matieus <mateuszolstowski@wp.pl>
2023-06-12 22:19:40 +0200

    add random modify

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <array>
 #include <random>
 
 
 using problem_t = std::vector<int>;
-using set_of_3_int = std::array<int, 3>;
+using set_of_3_int = std::vector<int>;
 using solution_t = std::vector<set_of_3_int>;
 
 std::random_device rd;
@@ -27,18 +26,18 @@ bool sum_of_problem_is_divided_by_3(int sum_of_problem) {
     return sum_of_problem % 3 == 0;
 }
 
-
-void show_solution_of_problem(solution_t solution) {
-    for (auto i: solution) {
-        // print solution by 1 set of 3
-    }
-}
-
 int sum_of_set(set_of_3_int set_of_3ints) {
     int sum = 0;
     for (auto i: set_of_3ints)
         sum += set_of_3ints[i];
     return sum;
+}
+
+
+void show_solution_of_problem(solution_t solution) {
+    for (auto set_of_3: solution) {
+        // std::cout << set_of_3 << sum_of_set(set_of_3) << std::endl;
+    }
 }
 
 
@@ -60,5 +59,12 @@ problem_t generate_random_problem(int number_of_sets_by_3, int min_rd, int max_r
     return problem_set;
 }
 
+problem_t random_modify(problem_t problem_set, int min_rd, int max_rd) {
+    std::uniform_int_distribution<int> random_idx(0, problem_set.size());
+    std::uniform_int_distribution<int> random_value(min_rd, max_rd);
+
+    problem_set[random_idx(rd)] += random_value(rd);
+    return problem_set;
+}
 
 int main() {}
