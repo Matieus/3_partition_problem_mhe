13de55f7c108b0acf13d75aa70acf4ab6b47f7ac
Matieus <mateuszolstowski@wp.pl>
2023-06-12 23:22:13 +0200

    add goal_function

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,36 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <memory>
 
+int sum_of_set(std::vector<int> set) {
+    int sum_of_numbers = 0;
+    for (int number: set)
+        sum_of_numbers += number;
+}
 
 using problem_t = std::vector<int>;
 using set_of_3_int = std::vector<int>;
-using solution_t = std::vector<set_of_3_int>;
+
+class solution_t : public std::vector<set_of_3_int> {
+    std::shared_ptr<problem_t> problem;
+
+    solution_t(std::shared_ptr<problem_t> problem_) : problem(problem_) {
+
+    }
+    /* void  goal(){
+        double result = 0;
+        for (int i = 0; i < this->size(); i++){
+            result
+
+        }
+        return result;
+    }*/
+};
 
 std::random_device rd;
 std::mt19937 gen(rd());
 
-
-int sum_of_problem(problem_t problem) {
-    int sum_of_numbers = 0;
-    for (int number: problem)
-        sum_of_numbers += number;
-}
 
 bool size_of_problem_is_divided_by_3(problem_t problem) {
     return problem.size() % 3 == 0;
@@ -24,13 +39,6 @@ bool size_of_problem_is_divided_by_3(problem_t problem) {
 
 bool sum_of_problem_is_divided_by_3(int sum_of_problem) {
     return sum_of_problem % 3 == 0;
-}
-
-int sum_of_set(set_of_3_int set_of_3ints) {
-    int sum = 0;
-    for (auto i: set_of_3ints)
-        sum += set_of_3ints[i];
-    return sum;
 }
 
 
@@ -49,11 +57,11 @@ problem_t generate_random_problem(int number_of_sets_by_3, int min_rd, int max_r
         problem_set.push_back(random(gen));
     }
 
-    int sum = sum_of_problem(problem_set);
+    int sum = sum_of_set(problem_set);
 
     while (sum % number_of_sets_by_3 != 0) {
         problem_set[random(rd)] += 1;
-        sum = sum_of_problem(problem_set);
+        sum = sum_of_set(problem_set);
     }
 
     return problem_set;
@@ -65,6 +73,16 @@ problem_t random_modify(problem_t problem_set, int min_rd, int max_rd) {
 
     problem_set[random_idx(rd)] += random_value(rd);
     return problem_set;
+}
+
+double goal_function(solution_t solution, int average) {
+    double result;
+
+    for (auto set_of_3: solution) {
+        if (sum_of_set(set_of_3) == average)
+            result += 1;
+    }
+    return result / solution.size();
 }
 
 int main() {}
